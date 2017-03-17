047205bcd7d4d820f67ad377eea371997994142a
Chris Coralli <ccoralli@gmail.com>
2017-03-17 18:51:32 -0400

    brain broken, did stuff.

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,19 @@ import 'angular-cookies';
 
 import AppConfig from './config';
 import SERVER from './server';
+import setup from './setup';
 
 import UserController from './controllers/user';
 import PhotoController from './controllers/photo';
-import CommentController from './controllers/comment'
+import CommentController from './controllers/comment';
+import LayoutController from './controllers/layout';
 
 angular
     .module('app', ['ui.router', 'ngCookies'])
     .config(AppConfig)
+    .run(setup)
     .constant('SERVER', SERVER)
     .controller('UserController', UserController)
     .controller('PhotoController', PhotoController)
-    .controller('CommentController', CommentController);
+    .controller('CommentController', CommentController)
+    .controller('LayoutController', LayoutController);
