8c45f1193c0753a27659fe8cf4526ce81a285131
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-03-17 18:56:05 -0400

    .

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ import SERVER from './server';
 
 import UserController from './controllers/user';
 import PhotoController from './controllers/photo';
-import CommentController from './controllers/comment'
+import CommentController from './controllers/comment';
 
 angular
     .module('app', ['ui.router', 'ngCookies'])
