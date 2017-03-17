d482c42b9c7583107e6a0e0c0d73478da43c70d0
Chris Coralli <ccoralli@gmail.com>
2017-03-17 16:21:06 -0400

    did some stuff.

--- src/js/controllers/comment.js
+++ src/js/controllers/comment.js
@@ -2,7 +2,8 @@ function CommentController ($scope, $http, SERVER, $state) {
     $scope.addComment = (data)  =>  {
         $http.post(`${SERVER}/photo/${$state.params.id}/comment`, data)
             .then(resp =>   {
-
+                console.log(resp);
+                $state.go('home.photos')
             })
     }
 }
